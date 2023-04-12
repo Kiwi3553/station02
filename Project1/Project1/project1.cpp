8a9879215ed7f616a49cb38211ee94627c821e39
EmotionalRealizer <tkdqor3553@naver.com>
2023-04-12 21:12:28 +0900

    첫커밋

---
+++ Project1/Project1/project1.cpp
@@ -0,0 +1,22 @@
+#include<studio.h>
+
+int main()
+{
+	//이것은 주석이다
+	//코딩하면 된다.
+	//이렇게 몸체 함수에 여러 함수를 넣으면 된다.
+	//코딩은 변수,함수의 집합이다.
+
+	//변수(변하는 수)
+
+	int flyheight = 0:  //정수형 변수 num을 선언(정의)한 후에 0을 넣겠다.
+
+	flyheight = 30; //정수형 변수 num에 3을 넣겠다
+	flyheight = 100: //정수형 변수 num에 3을 넣겠다.
+
+	int power = 0;
+	power = 1:
+
+	float fnum = 0.0;
+
+	fnum = 3.5;
