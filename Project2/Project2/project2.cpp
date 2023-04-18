ec8cd352f9b576bd4f25b115444a15042de38538
EmotionalRealizer <tkdqor3553@naver.com>
2023-04-18 10:55:40 +0900

    주석의종류

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -2,6 +2,25 @@
 
 int main()
 {
-	printf("안녕하세요");
+	//이것은 주석이다.
+	/*이것도 주석이다.*/
+	//이렇게 메인 함수에 여러 함수와 변수를 넣으면된다
+	
+	//변수(변하는 수)
+
+	//int flyheight = 0;
+	/*정수형 벼수 num을 선언(정의)한 후에 0을 넣겠다*/
+
+	//flyheight = 30; //정수형 변수 num에 30을 넣겠다
+
+	//int power = 0;
+	//power = 1;
+
+	//float fnum = 0.0;
+
+	//fnum = 3.5;
+
+
+	printf("Hello World!");
 	return 0;
 }
