1a66aa6192b173e4ea9ce2c23cc07cf8b5216023
EmotionalRealizer <tkdqor3553@naver.com>
2023-04-19 10:45:07 +0900

    정수형 형식지정자써보기

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -21,6 +21,23 @@ int main()
 	//fnum = 3.5;
 
 
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+
+
+	int num1 = 3;
+	int num2 = 5;
+
+	num1 = num2;
+	num2 = 0;
+
+	int a;
+	int b;
+	int c;
+	a = 10;
+	b = 20;
+	c = 30;
+
+	printf("a 는 %d이고 b 는 %d이고 c 는 %d입니다.", a, b, c);
 	return 0;
 }
