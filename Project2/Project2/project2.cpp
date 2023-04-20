0ea04d7f7bddd7346a7607405497abd01661fa70
EmotionalRealizer <tkdqor3553@naver.com>
2023-04-20 19:11:22 +0900

    저축액을 계산하는 프로그램 제작

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -47,7 +47,7 @@ int main()
 	printf("d 는 %f 입니다.\n", d);
 	printf("e 는 %f 입니다.\n", e); */
 
-	int x;
+	/*int x;
 	int y;
 	int sum;
 
@@ -58,7 +58,18 @@ int main()
 	scanf("%d",&y);
 
 	sum = x + y;
-	printf("두수의 합: %d", sum);
+	printf("두수의 합: %d", sum);*/
+
+	/*저축액을 계산하는 프로그램*/
+	int salary;       //월급
+	int deposit;      //저축액
+
+	printf("월급을 입력하시오:");
+	scanf("%d", &salary);
+
+	deposit = 10 * 12 * salary;
+
+	printf("10년 동안의 저축액: %d\n", deposit);
 
 	
 	return 0;
