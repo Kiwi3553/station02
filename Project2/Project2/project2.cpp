0cf04eb244f047ecc67941378edd61e38084490f
EmotionalRealizer <tkdqor3553@naver.com>
2023-05-03 22:35:06 +0900

    윤년 프로그램

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -255,12 +255,20 @@ int main()
 	printf("경고를 출력해보자'\\007'을 출력한다 \007 \n"); 
 	*/
 
-	/*연산자 실습*/
+	/*연산자 실습
 	int num = 3;
 
 	num++;
 
 	printf("%d", num);
+	*/
+	
+	/*윤년 프로그램*/
+	int year, result;
+	printf("연도를 입력하시오: ");
+	scanf("%d", &year);
+	result = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+	printf("result=%d \n", result);
 
 	return 0;
 }
