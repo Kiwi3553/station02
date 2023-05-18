2cf41c190fa3873932274e31022c4a87f405625b
EmotionalRealizer <tkdqor3553@naver.com>
2023-05-18 16:43:14 +0900

    break을 이용하여 학생의 성적을 합산하고 평균을 산출하는 무한루프 탈출하기

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -901,7 +901,7 @@ for (s = 0; s < 20; s++)
 }
 */
 
-/*break을 이용하여 실수의 제곱근을 출력하는 무한루프 탈출하기*/
+/*break을 이용하여 실수의 제곱근을 출력하는 무한루프 탈출하기
 
 double v;
 
@@ -914,6 +914,26 @@ while (1)
 		break;
 	printf("%lf의 제곱근은 %lf입니다.\n", v, sqrt(v));
 }
+*/
+
+/*break을 이용하여 학생의 성적을 합산하고 평균을 산출하는 무한루프 탈출하기*/
+
+float grade, sum = 0, average; 
+int count = 0;  //과목수 or 평균의 기준 요소
+
+while (1)
+{
+	printf("학생 성적을 입력하시오:");
+	scanf("%f", &grade);
+	if (grade < 0)
+		break;
+	count++;
+	sum += grade;
+
+}
+average = sum / count;
+printf("학생들의 성적의 평균은 %f입니다.\n", average);
+
 return 0;
 
 
